4967a387249f05aa745825d94c740e8025a90cc1
nina75 <kozasbrada28>
2016-04-06 11:08:08 +0300

    Task 14 Current date and time

---
+++ IntoductionToProgramming/CurrentDateAndTime/CurrentTime.cs
@@ -0,0 +1,11 @@
+﻿//Create a console application that prints the current date and time. Find out how in Internet.
+
+using System;
+
+class CurrentTime
+{
+    static void Main()
+    {
+        System.Console.WriteLine(DateTime.Now);
+    }
+}
