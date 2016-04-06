fec7b7405d5e23dcc789805a43aec9dec832dfa5
nina75 <kozasbrada28>
2016-04-06 08:57:52 +0300

    Task 2 Blank solution

---
+++ IntoductionToProgramming/BlankSolution/Solution.cs
@@ -0,0 +1,10 @@
+﻿//Create a blank solution in Visual Studio called “Intro-Programming-Homework”. use this solution to hold all your homework projects,
+//code and files. For each problem (exercises) add a separate project with self-descriptive name like Hello-World and Print-Your-Name.
+
+class Solution
+{
+    static void Main()
+    {
+        //nothing to do :)
+    }
+}
