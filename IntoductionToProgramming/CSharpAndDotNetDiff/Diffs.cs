56d54abed22a056c27e4ca2b12bafc38796ad622
nina75 <kozasbrada28>
2016-04-06 11:05:13 +0300

    Task 13 Difference between .NET Framework and C#

---
+++ IntoductionToProgramming/CSharpAndDotNetDiff/Diffs.cs
@@ -0,0 +1,11 @@
+﻿//Describe the difference between C# and .NET Framework in 2-3 sentences.
+
+using System;
+
+class Diffs
+{
+    static void Main()
+    {
+        Console.WriteLine(".NET is Environment for execution of .NET programs, Powerful library of classes, Common execution engine for many programming languages like C#, F#, Visual Basic and many other");
+    }
+}
