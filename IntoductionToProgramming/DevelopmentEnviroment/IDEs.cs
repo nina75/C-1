6cfd13f9000e7fd9a183293b745f890db12c55b2
nina75 <kozasbrada28>
2016-04-06 10:55:28 +0300

    Task 12 IDEs

---
+++ IntoductionToProgramming/DevelopmentEnviroment/IDEs.cs
@@ -0,0 +1,10 @@
+﻿//Perform a research (e.g. in Google or Wikipedia) and provide a short list with popular development environments (IDEs) like Visual Studio.
+//Write in a text file called list-of-IDEs.txt at least five IDEs along with 2-3 sentences about each of them.Use English.
+
+class IDEs
+{
+    static void Main()
+    {
+        System.Console.WriteLine("WebStorm, PHPStorm, NetBeans, IntelliJ IDEA, CodeBlocks");
+    }
+}
