761d0a3dcf9b906b0fad0696d361838acf5986d8
nina75 <kozasbrada28>
2016-04-06 09:30:55 +0300

    Task 7 Print Full Name

---
+++ IntoductionToProgramming/PrintFirstAndLastName/FirstAndLastName.cs
@@ -0,0 +1,12 @@
+﻿//Create console application that prints your first and last name, each at a separate line.
+
+using System;
+
+class FirstAndLastName
+{
+    static void Main()
+    {
+        Console.WriteLine("Pesho");
+        Console.WriteLine("Petrov");
+    }
+}
