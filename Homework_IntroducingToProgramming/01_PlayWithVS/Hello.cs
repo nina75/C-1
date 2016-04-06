12b8248115d3e8c02e96a08253e04c72b76b9bfc
nina75 <kozasbrada28>
2016-04-06 06:04:43 +0300

    Adding new row for testing credentials

---
+++ Homework_IntroducingToProgramming/01_PlayWithVS/Hello.cs
@@ -0,0 +1,13 @@
+﻿namespace _01_PlayWithVS
+{
+    using System;
+
+    public class Hello
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, my name is Pesho");
+            Console.WriteLine("Hello, my name is Pesho");
+        }
+    }
+}
