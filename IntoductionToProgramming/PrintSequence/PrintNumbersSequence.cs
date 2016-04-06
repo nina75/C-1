770436441acb26a82fda65f14920489ef51eb0f5
nina75 <kozasbrada28>
2016-04-06 09:42:55 +0300

    Task 9 Print sequence

---
+++ IntoductionToProgramming/PrintSequence/PrintNumbersSequence.cs
@@ -0,0 +1,15 @@
+﻿//Write a program that prints the first 10 members of the sequence: 2, -3, 4, -5, 6, -7, ...
+using System;
+
+class PrintNumbersSequence
+{
+    static void Main()
+    {
+        int multiplier = 1;
+        for (int i = 2; i < 12; i++)
+        {
+            Console.WriteLine(i * multiplier);
+            multiplier *= -1;
+        }
+    }
+}
