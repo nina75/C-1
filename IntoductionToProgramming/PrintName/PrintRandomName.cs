0e90a4492333c585579b256a71c2927c7ea36695
nina75 <kozasbrada28>
2016-04-06 09:17:19 +0300

    Task 5 Print Name

---
+++ IntoductionToProgramming/PrintName/PrintRandomName.cs
@@ -0,0 +1,11 @@
+﻿//Create a C# application that print your name on the console.
+//Ensure you have named the application well(e.g. “PrintMyName”).
+using System;
+
+class PrintRandomName
+{
+    static void Main()
+    {
+        Console.WriteLine("Pesho Petrov");
+    }
+}
