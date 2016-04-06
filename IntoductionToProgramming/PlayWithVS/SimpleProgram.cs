8459db2ffd10650be0f312daa1c90f7cbf050d8f
nina75 <kozasbrada28>
2016-04-06 08:53:02 +0300

    Start homework

---
+++ IntoductionToProgramming/PlayWithVS/SimpleProgram.cs
@@ -0,0 +1,12 @@
+﻿// Install Visual Studio on your laptop or home computer.Go to the official Visual Studio site and download the Community 2015 version.
+// Start Visual Studio and familiarize yourself with it.Create a simple C# program (console application), compile and run it.
+
+using System;
+
+class SimpleProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("Pesho");
+    }
+}
