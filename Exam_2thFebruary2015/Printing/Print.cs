85a618235c34d274b732ad0ce61265e5ee5513af
nina75 <kozasbrada28>
2016-04-08 01:09:09 +0300

    Task 1 Printing

---
+++ Exam_2thFebruary2015/Printing/Print.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Print
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int S = int.Parse(Console.ReadLine());
+        double P = double.Parse(Console.ReadLine());
+
+        long totalSheets = N * S;
+        double reamsCount = totalSheets / 500d;
+        double result = reamsCount * P;
+        Console.WriteLine("{0:F2}", result);
+    }
+}
