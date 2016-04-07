286bae8e373f07edb2902f12b60b29f630c54f59
nina75 <kozasbrada28>
2016-04-08 01:57:06 +0300

    Task 2 Text to Number

---
+++ Exam_2thFebruary2015/TextToNumber/Text.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Text
+{
+    static void Main()
+    {
+        int M = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+        int result = 0;
+       
+          
+        for (int i = 0; i < text.Length; i++)
+        {
+            char currentSymbol = text[i];
+            if (currentSymbol == '@')
+            {
+                Console.WriteLine(result);
+                break;
+            }
+            else if (Char.IsDigit(currentSymbol))
+            {
+                result *= currentSymbol - '0';
+            }
+            else if (Char.IsLetter(currentSymbol))
+            {
+                result += currentSymbol.ToString().ToUpper().ToCharArray()[0] - 'A';
+            }
+            else if (true)
+            {
+                result = result % M;
+            }
+        }
+    }
+}
